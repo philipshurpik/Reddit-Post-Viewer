b3c51e5850140c507c0436798b0a503912ded413
Philip Shurpik <Philip.Shurpik@KruscheCompany.com>
2015-04-20 22:15:55 +0300

    Added Navbar button

--- src/Main/NavBar.js
+++ src/Main/NavBar.js
@@ -1,15 +1,10 @@
 var React = require('react');
 var Bootstrap = require('react-bootstrap');
-var { Nav, NavItem, Navbar, Input, Col } = Bootstrap;
+var { Nav, NavItem, Navbar, Input, Col, Button } = Bootstrap;
 
 var NavBar = React.createClass({
-    getInitialState() {
-        return {subreddit: ""};
-    },
-    componentWillReceiveProps() {
-        if (this.props.subreddit && this.props.subreddit.length > 0) {
-            this.setState({subreddit: this.props.subreddit});
-        }
+    contextTypes: {
+        router: React.PropTypes.func
     },
     render: function () {
         var link = (this.props.subreddit ? ("/#/" + this.props.subreddit + "/t/") : "/#/t/");
@@ -21,9 +16,19 @@ var NavBar = React.createClass({
                 <NavItem eventKey={1} href={link + "controversial"}>Controversial</NavItem>
             </Nav>
             <Col xs={3}>
-                <Input type='text' placeholder='Subreddit' value={this.state.subreddit} />
+                <Input type='text' placeholder='Subreddit'
+                    onChange={(evt) => this.setState({newSubReddit: evt.target.value})}
+                />
+            </Col>
+            <Col xs={1}>
+                <Button onClick={this.handleEnter}>Go!</Button>
             </Col>
         </Navbar>;
+    },
+    handleEnter: function() {
+        if (this.state.newSubReddit.trim().length > 0) {
+            this.context.router.transitionTo("/" + this.state.newSubReddit);
+        }
     }
 });
 
